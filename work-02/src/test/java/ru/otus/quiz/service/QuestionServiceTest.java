9f6efe07ec8cc86d533f951b1c172355d760c95e
chesbor <chesbor@gmail.com>
2021-07-14 00:26:51 +0300

    Убрал спрингбут с тестов

--- work-02/src/test/java/ru/otus/quiz/service/QuestionServiceTest.java
+++ work-02/src/test/java/ru/otus/quiz/service/QuestionServiceTest.java
@@ -2,17 +2,21 @@ package ru.otus.quiz.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.quiz.Application;
 import ru.otus.quiz.configuration.QuizConfiguration;
 import ru.otus.quiz.dao.QuestionReader;
 import ru.otus.quiz.domain.Question;
 
-@SpringBootTest(classes = Application.class)
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = Application.class)
 @TestPropertySource("classpath:application-test.properties")
 class QuestionServiceTest {
     @Autowired
--- work-02/src/test/java/ru/otus/quiz/service/StudentServiceTest.java
+++ work-02/src/test/java/ru/otus/quiz/service/StudentServiceTest.java
@@ -2,15 +2,19 @@ package ru.otus.quiz.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.quiz.Application;
 import ru.otus.quiz.domain.Student;
 
-@SpringBootTest(classes = Application.class)
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = Application.class)
 @TestPropertySource("classpath:application-test.properties")
 class StudentServiceTest {
     private final static String DEFAULT_NAME = "Sergey";
